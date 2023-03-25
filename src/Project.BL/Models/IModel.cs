68603d600403b315ebc3c4a06f9d7a3e4faaf2ec
elafed <fedorovaela2@gmail.com>
2023-03-25 15:24:50 +0100

    added structure for facades and models

---
+++ src/Project.BL/Models/IModel.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+
+namespace Project.BL.Models;
+
+public interface IModel
+{
+    Guid Id { get; }
+}
---
+++ src/Project.BL/Models/ModelBase.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project.BL.Models;
+
+public abstract record ModelBase : INotifyPropertyChanged, IModel
+{
+    public Guid Id { get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
