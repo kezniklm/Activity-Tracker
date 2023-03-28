2e434dd964d6241161c10eb94755a8148a6309f3
Marián Tarageľ <xtarag01@vutbr.cz>
2023-03-28 21:17:28 +0200

    Added repository tests base

---
+++ src/Project.DAL.Tests/RepositoryTests/RepositoryTestsBase.cs
@@ -0,0 +1,27 @@
+﻿namespace Project.DAL.Tests.RepositoryTests;
+
+public class RepositoryTestsBase : IAsyncLifetime
+{
+    protected ProjectDbContext DbContext;
+    protected IDbContextFactory<ProjectDbContext> DbContextFactory { get; }
+    protected Repository<UserEntity> RepositorySUT { get; set; }
+
+    public RepositoryTestsBase()
+    {
+        DbContextFactory = new DbContextSqLiteFactory(GetType().FullName!);
+        DbContext = DbContextFactory.CreateDbContext();
+        RepositorySUT = new Repository<UserEntity>(DbContext, new UserEntityMapper());
+    }
+
+    public async Task InitializeAsync()
+    {
+        await DbContext.Database.EnsureDeletedAsync();
+        await DbContext.Database.EnsureCreatedAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await DbContext.Database.EnsureDeletedAsync();
+        await DbContext.DisposeAsync();
+    }
+}
--- src/Project.DAL.Tests/Usings.cs
+++ src/Project.DAL.Tests/Usings.cs
@@ -4,3 +4,5 @@ global using Project.DAL.Factories;
 global using Project.DAL.Entities;
 global using Project.Common.Tests;
 global using Microsoft.EntityFrameworkCore;
+global using Project.DAL.Mappers;
+global using Project.DAL.Repositories;
