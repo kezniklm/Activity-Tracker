e1c565ffa0c07df0b859a1135fedd9873f9e7e6f
Simona Jánošíková <xjanos19@vutbr.cz>
2023-04-06 16:46:22 +0200

    added Repository Project test

---
+++ src/Project.DAL.Tests/RepositoryTests/RepositoryProjectTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Project.DAL.Tests.RepositoryTests;
+public class RepositoryProjectTests : RepositoryTestsBase
+{
+    [Fact]
+    public async Task GetOneProject()
+    {
+        // Setup
+        ProjectEntity projectEntity = new() { Id = Guid.NewGuid(), Name = "Projekt1"};
+        DbContext.Projects.Add(projectEntity);
+        await DbContext.SaveChangesAsync();
+
+        // Exercise
+        ProjectEntity actualEntity = RepositorySPT.GetOne(projectEntity.Id);
+
+        // Verify
+        Assert.Equal(projectEntity, actualEntity);
+    }
+
+    [Fact]
+    public async Task UpdateProject()
+    {
+        // Setup
+        ProjectEntity projectEntity = new() { Id = Guid.NewGuid(), Name = "Projekt2"};
+        DbContext.Projects.Add(projectEntity);
+        await DbContext.SaveChangesAsync();
+        ProjectEntity updateProjectEntity = new()
+        {
+            Id = projectEntity.Id,
+            Name = projectEntity.Name
+        };
+
+        // Exercise
+        await RepositorySPT.UpdateAsync(updateProjectEntity);
+
+        // Verify
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntity = await dbx.Projects.SingleAsync(i => i.Id == projectEntity.Id);
+        Assert.NotEqual(projectEntity, actualEntity);
+        Assert.Equal(updateProjectEntity.Name, actualEntity.Name);
+    }
+
+    [Fact]
+    public async Task RemoveProject()
+    {
+        // Setup
+        ProjectEntity projectEntity = new() { Id = Guid.NewGuid(), Name = "Projekt1"};
+        DbContext.Projects.Add(projectEntity);
+        await DbContext.SaveChangesAsync();
+
+        // Exercise
+        RepositorySPT.Delete(projectEntity.Id);
+
+        // Verify
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        Assert.False(await dbx.Projects.AnyAsync(i => i.Name == projectEntity.Name));
+    }
+
+    [Fact]
+    public async Task AddProject()
+    {
+        // Setup
+        ProjectEntity projectEntity = new() { Id = Guid.NewGuid(), Name = "Projekt1" };
+
+        // Exercise
+        await RepositorySPT.InsertAsync(projectEntity);
+
+        // Verify
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntity = await dbx.Projects.SingleOrDefaultAsync(i => i.Id == projectEntity.Id);
+        DeepAssert.Equal(projectEntity, actualEntity);
+    }
+}
--- src/Project.DAL.Tests/RepositoryTests/RepositoryTestsBase.cs
+++ src/Project.DAL.Tests/RepositoryTests/RepositoryTestsBase.cs
@@ -5,12 +5,14 @@ public class RepositoryTestsBase : IAsyncLifetime
     protected ProjectDbContext DbContext;
     protected IDbContextFactory<ProjectDbContext> DbContextFactory { get; }
     protected Repository<UserEntity> RepositorySUT { get; set; }
+    protected Repository<ProjectEntity> RepositorySPT { get; set; }
 
     public RepositoryTestsBase()
     {
         DbContextFactory = new DbContextSqLiteFactory(GetType().FullName!);
         DbContext = DbContextFactory.CreateDbContext();
         RepositorySUT = new Repository<UserEntity>(DbContext, new UserEntityMapper());
+        RepositorySPT = new Repository<ProjectEntity>(DbContext, new ProjectEntityMapper());
     }
 
     public async Task InitializeAsync()
