8f8f20e6c089037a342b06c240736d62bc4c6923
xsmole02 <xsmole@vutbr.cz>
2023-04-09 21:59:29 +0200

    Added UserProjectFacadeTests

--- src/Project.BL/Facades/Interfaces/IUserProjectFacade.cs
+++ src/Project.BL/Facades/Interfaces/IUserProjectFacade.cs
@@ -4,4 +4,5 @@ using Project.DAL.Entities;
 namespace Project.BL.Facades.Interfaces;
 public interface IUserProjectFacade : IFacadeBase<UserProjectEntity, UserProjectListModel, UserProjectDetailModel>
 {
+    Task<UserProjectDetailModel> SaveAsync(UserProjectDetailModel model);
 }
