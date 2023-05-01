eb7be8c744bafece672a188c1e328f5e39cb204f
Simona Jánošíková <xjanos19@vutbr.cz>
2023-05-01 22:28:02 +0200

    edited name of Page and changes activity list styles

--- src/Project.App/App.xaml.cs
+++ src/Project.App/App.xaml.cs
@@ -8,4 +8,13 @@ public partial class App : Application
 
         MainPage = serviceProvider.GetRequiredService<AppShell>();
     }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var view = base.CreateWindow(activationState);
+        view.Width = 1100;
+        view.Height = 600;
+        view.MinimumWidth = 1100;
+        return view;
+    }
 }
